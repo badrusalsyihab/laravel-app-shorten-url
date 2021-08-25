d213dce0fc0ab2783f4a452d4d9235add1b7cf50
myqdh <badrusalsyihab@gmail.com>
2021-08-25 11:17:34 +0700

    add  model, controller & route

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'ShortLinkController@index');  
+Route::post('generate-link', 'ShortLinkController@store')->name('generate.shorten.link');  
+Route::get('{code}', 'ShortLinkController@shortenLinkDetail')->name('shorten.link');  
